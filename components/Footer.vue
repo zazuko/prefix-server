89e15f138998768d60b1df8adc9796bdeb6fd8da
Victor Felder <victor@draft.li>
2019-06-24 21:17:26 +0200

    feat: add footer

---
+++ components/Footer.vue
@@ -0,0 +1,24 @@
+<template>
+  <footer class="footer-container">
+    <div class="layout-width">
+      <div class="footer">
+        <div class="footer-link">
+          <nav class="nav-group">
+            <div class="nav-item">
+              <router-link to="/namespaces" class="nav-link">
+                Vocabularies
+              </router-link>
+            </div>
+            <div class="nav-item">
+              <router-link to="/api" class="nav-link">
+                API
+              </router-link>
+            </div>
+          </nav>
+        </div> <div class="footer-copyright">
+          Copyright <a href="http://zazuko.com">Zazuko</a> GmbH © 2019
+        </div>
+      </div>
+    </div>
+  </footer>
+</template>
