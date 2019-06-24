887f43ebd44175ba2d2713f17b2d60935b894042
Victor Felder <victor@draft.li>
2019-06-24 21:08:21 +0200

    fix: better clipboard location

--- components/MainResults.vue
+++ components/MainResults.vue
@@ -1,11 +1,33 @@
 <template>
   <section>
+    <v-alert
+      :value="copySuccess"
+      type="success"
+      dismissible
+      transition="scale-transition"
+    >
+      Copied!
+    </v-alert>
+    <v-alert
+      :value="copyFailure"
+      type="error"
+      dismissible
+      transition="scale-transition"
+    >
+      Could not copy to clipboard, sorry!
+    </v-alert>
     <div class="big">
       <p>
         <span class="partA">{{ model.prefixedSplitA }}:</span>
         <span class="partB">{{ model.prefixedSplitB }}</span>
       </p>
       <p>
+        <clipboard
+          v-if="model"
+          :to-copy="model.iri.value"
+          @success="clipboardSuccessHandler"
+          @error="clipboardErrorHandler"
+        />
         <span class="partA">{{ model.iriSplitA }}</span>
         <span class="partB">{{ model.iriSplitB }}</span>
       </p>
@@ -28,12 +50,37 @@
 </template>
 
 <script>
+import Clipboard from '@/components/Clipboard'
+
 export default {
   name: 'MainResults',
+  components: {
+    Clipboard
+  },
   props: {
     model: {
       type: Object,
       required: true
+    }
+  },
+  data() {
+    return {
+      copySuccess: false,
+      copyFailure: false
+    }
+  },
+  methods: {
+    clipboardSuccessHandler() {
+      this.copySuccess = true
+      setTimeout(() => {
+        this.copySuccess = false
+      }, 2000)
+    },
+    clipboardErrorHandler() {
+      this.copyFailure = true
+      setTimeout(() => {
+        this.copyFailure = false
+      }, 2000)
     }
   }
 }
