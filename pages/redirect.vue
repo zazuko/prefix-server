17bedf22d297cc9a21057be9a6902f3151e5f9e9
Quentin Gliech <quentingliech@gmail.com>
2019-06-27 11:14:34 +0200

    feat(autocomplete): Search on enter with a form

---
+++ pages/redirect.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+  name: 'Redirect',
+  asyncData ({ redirect, query }) {
+    if (query.q) {
+      redirect('/' + query.q)
+    }
+    else {
+      redirect('/')
+    }
+  }
+}
+</script>
