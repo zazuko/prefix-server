588c31f35091368c5edee3044e110b3967f3feed
Quentin Gliech <quentingliech@gmail.com>
2019-06-27 11:14:57 +0200

    test(autocomplete): Fix e2e tests for the new autocompletion widget

--- test/e2e/integration/home_spec.js
+++ test/e2e/integration/home_spec.js
@@ -10,9 +10,19 @@ const collectResults = (result) => {
 }
 
 const searchField = () => cy.get('.search-field-container input')
-const suggestionList = () => cy.get('div.v-list')
-const suggestedElements = () => cy.get('div.v-list .v-list__tile__title')
-const highlightedResult = () => cy.get('div.v-list .v-list__tile--highlighted .v-list__tile__title')
+const suggestionList = () => cy.get('.autocomplete .results')
+const suggestedElements = () => cy.get('.autocomplete .results li')
+const keys = {
+  generic: x => cy.focused().trigger('keydown', { keyCode: x, which: x }),
+  downArrow: () => keys.generic(40),
+  upArrow: () => keys.generic(38),
+  repeat (what, count, time = 10) {
+    for (let i = 0; i < count; i++) {
+      what()
+      cy.wait(time)
+    }
+  }
+}
 
 describe('Search', () => {
   before(() => {
@@ -28,30 +38,36 @@ describe('Search', () => {
     suggestionList().should('be.visible')
     suggestedElements().then(collectResults)
     searchField().clear()
+    suggestionList().should('not.be.visible')
 
     searchField().type('Person')
     cy.wait(1500)
     suggestionList().should('be.visible')
     suggestedElements().then(collectResults)
     searchField().clear()
+    suggestionList().should('not.be.visible')
 
     searchField().type('rdfs')
     cy.wait(1500)
     suggestionList().should('be.visible')
     suggestedElements().then(collectResults)
     searchField().clear()
+    suggestionList().should('not.be.visible')
   })
 
   it('should navigate results with keyboard', () => {
     searchField().type('schema:P')
     suggestionList().should('be.visible')
-    searchField().type('{downarrow}{downarrow}{downarrow}{downarrow}{downarrow}{downarrow}')
-    highlightedResult().contains('schema:ParentAudience')
-    searchField().type('{uparrow}{uparrow}{uparrow}{downarrow}')
-    highlightedResult().contains('schema:ParcelDelivery')
-    searchField().type('{uparrow}{downarrow}{uparrow}')
-    highlightedResult().contains('schema:Paperback')
-    searchField().type('{enter}')
+    keys.repeat(keys.downArrow, 6)
+    cy.focused().contains('schema:ParentAudience')
+    keys.repeat(keys.upArrow, 3)
+    keys.downArrow()
+    cy.focused().contains('schema:ParcelDelivery')
+    keys.upArrow()
+    keys.downArrow()
+    keys.upArrow()
+    cy.focused().contains('schema:Paperback')
+    cy.focused().click()
     cy.url().should('include', '/schema:Paperback')
   })
 })
