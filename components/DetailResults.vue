2872a566d780884af40e87c8f1ac04b0271d833b
Victor Felder <victor@draft.li>
2019-06-27 13:45:41 +0200

    feat: SSR results page; project works with JS disabled

--- components/DetailResults.vue
+++ components/DetailResults.vue
@@ -1,22 +1,22 @@
 <template>
   <section class="grid">
-    <template v-for="predicate in importantPredicates">
-      <template v-if="prefixedPredicates[predicate]">
+    <template v-for="predicate in predicates.importantPredicates">
+      <template v-if="predicates.prefixedPredicates[predicate]">
         <div :key="predicate" class="row">
           <predicate :term="predicate" />
-          <terms :terms="prefixedPredicates[predicate]" />
+          <terms :terms="predicates.prefixedPredicates[predicate]" />
         </div>
       </template>
     </template>
 
-    <template v-for="item in sortedPrefixedPredicates">
+    <template v-for="item in predicates.sortedPrefixedPredicates">
       <div :key="item.predicate" class="row">
         <predicate :term="item.predicate" />
         <terms :terms="item.values" />
       </div>
     </template>
 
-    <template v-for="item in sortedIriPredicates">
+    <template v-for="item in predicates.sortedIriPredicates">
       <div :key="item.predicate" class="row">
         <predicate :term="item.predicate" />
         <terms :terms="item.values" />
@@ -59,49 +59,45 @@ export default {
       required: true
     }
   },
-  data () {
-    return {
-      // important predicates, manually selected+sorted
-      importantPredicates,
-      // the ones that are prefixed, alphabetically sorted
-      prefixedPredicates: {},
-      // other predicates, alphabetically sorted
-      sortedIriPredicates: {},
-      // tmp
-      iriPredicates: {},
-      sortedPrefixedPredicates: []
+  computed: {
+    predicates () {
+      const [prefixedPredicates, iriPredicates] = this.model.parts
+        .reduce(([prefixedPredicates, iriPredicates], field) => {
+          if (field.predicate !== field.predicateIRI) {
+            if (!prefixedPredicates[field.predicate]) {
+              prefixedPredicates[field.predicate] = []
+            }
+            prefixedPredicates[field.predicate].push(field)
+          }
+          else {
+            if (!iriPredicates[field.predicate]) {
+              iriPredicates[field.predicate] = []
+            }
+            iriPredicates[field.predicate].push(field)
+          }
+          return [prefixedPredicates, iriPredicates]
+        }, [{}, {}])
+
+      const sortedPrefixedPredicates = _sortBy(
+        Object
+          .entries(prefixedPredicates)
+          .filter(([predicate, value]) => !(importantPredicates.includes(predicate)))
+          .map(([predicate, values]) => ({ predicate, values }))
+        , 'predicate')
+
+      const sortedIriPredicates = _sortBy(
+        Object
+          .entries(iriPredicates)
+          .map(([predicate, values]) => ({ predicate, values }))
+        , 'predicate')
+
+      return {
+        prefixedPredicates,
+        importantPredicates,
+        sortedPrefixedPredicates,
+        sortedIriPredicates
+      }
     }
-  },
-  mounted () {
-    [this.prefixedPredicates, this.iriPredicates] = this.model.parts
-      .reduce(([prefixedPredicates, iriPredicates], field) => {
-        if (field.predicate !== field.predicateIRI) {
-          if (!prefixedPredicates[field.predicate]) {
-            prefixedPredicates[field.predicate] = []
-          }
-          prefixedPredicates[field.predicate].push(field)
-        }
-        else {
-          if (!iriPredicates[field.predicate]) {
-            iriPredicates[field.predicate] = []
-          }
-          iriPredicates[field.predicate].push(field)
-        }
-        return [prefixedPredicates, iriPredicates]
-      }, [{}, {}])
-
-    this.sortedPrefixedPredicates = _sortBy(
-      Object
-        .entries(this.prefixedPredicates)
-        .filter(([predicate, value]) => !(importantPredicates.includes(predicate)))
-        .map(([predicate, values]) => ({ predicate, values }))
-      , 'predicate')
-
-    this.sortedIriPredicates = _sortBy(
-      Object
-        .entries(this.iriPredicates)
-        .map(([predicate, values]) => ({ predicate, values }))
-      , 'predicate')
   }
 }
 </script>
--- pages/_.vue
+++ pages/_.vue
@@ -1,25 +1,180 @@
 <template>
-  <search :prefetched-entries="prefetchedEntries" />
+  <div class="main-container">
+    <div class="home-header">
+      <div class="search-field-container flex-container">
+        <div class="flex-item title">
+          <div>
+            <h1>
+              Resolve
+              <br>
+              RDF Terms
+            </h1>
+          </div>
+        </div>
+        <div class="flex-item">
+          <autocomplete
+            :entries="entries"
+            :search-input.sync="search" />
+        </div>
+        <div class="flex-item desc">
+          <div>
+            Data based on <a href="https://github.com/zazuko/rdf-vocabularies">@zazuko/rdf-vocabularies</a>
+          </div>
+          <div class="tail"></div>
+        </div>
+      </div>
+    </div>
+
+    <div v-if="model" class="main-results">
+      <main-results :model="model" />
+    </div>
+
+    <div class="search-results">
+      <detail-results
+        v-if="model"
+        :model="model" />
+    </div>
+  </div>
 </template>
 
 <script>
-import Search from '@/components/Search'
+import _debounce from 'lodash/debounce'
+import MainResults from '@/components/MainResults'
+import DetailResults from '@/components/DetailResults'
+import Autocomplete from '@/components/Autocomplete'
+
+function pickFromEntries (iriFromURL, entries) {
+  if (!iriFromURL || !Array.isArray(entries)) {
+    return false
+  }
+
+  // find the best match from the search results
+  for (const match of entries) {
+    // ideally a case sensitive exact match
+    if (
+      match.iri.value === iriFromURL ||
+      match.prefixed === iriFromURL
+    ) {
+      return match
+    }
+  }
+
+  // otherwise a case insensitive one
+  for (const match of entries) {
+    if (
+      match.iri.value.toLowerCase() === iriFromURL.toLowerCase() ||
+      match.prefixed.toLowerCase() === iriFromURL.toLowerCase()
+    ) {
+      return match
+    }
+  }
+  return false
+}
 
 export default {
   components: {
-    Search
+    MainResults,
+    DetailResults,
+    Autocomplete
+  },
+  data () {
+    return {
+      isLoading: false,
+      model: null,
+      search: null,
+      iriFromURL: ''
+    }
+  },
+  computed: {
+    items () {
+      return this.entries
+    }
+  },
+  watch: {
+    model () {
+      // only redirect to result when user search something, otherwise people hitting
+      // http://localhost:3000/http://www.w3.org/ns/rdfa#PrefixOrTermMapping
+      // will also be redirected
+      if (this.model && this.search) {
+        this.$router.push(`/${this.model.prefixed}`)
+      }
+    },
+    search: _debounce(async function (val) {
+      await this.doSearch(val)
+    }, 250)
   },
   async asyncData ({ $axios, params, error }) {
     let entries = []
-    if (params.pathMatch) {
-      const val = `${params.pathMatch}`.replace(/#/g, '---hash---')
+    const iriFromURL = params.pathMatch
+    if (iriFromURL) {
+      const val = `${iriFromURL}`.replace(/#/g, '---hash---')
       entries = await $axios.$get(`/api/v1/search?q=${val}`)
+      const match = pickFromEntries(iriFromURL, entries)
+      if (match) {
+        return {
+          model: match,
+          entries: []
+        }
+      }
       if (!entries.length) {
         return error({ statusCode: 404, message: 'No Result' })
       }
     }
     return {
-      prefetchedEntries: entries
+      entries
+    }
+  },
+  async mounted () {
+    this.iriFromURL = this.$route.path + this.$route.hash
+    if (this.iriFromURL.startsWith('/')) {
+      // this.$route.path often starts with `/`, strip it since
+      // no IRI nor any prefix start with /
+      this.iriFromURL = this.iriFromURL.substr(1)
+    }
+
+    const match = pickFromEntries(this.iriFromURL, this.entries)
+    if (match) {
+      this.model = match
+      this.entries = []
+      return
+    }
+
+    await this.doSearch(this.iriFromURL)
+    if (this.entries.length) {
+      this.model = this.entries[0]
+      this.entries = []
+    }
+  },
+  methods: {
+    async doSearch (val) {
+      val = val || ''
+      val = val.replace(/#/g, '---hash---')
+      if (val.toLowerCase() === this.loadingVal) {
+        // Items have already been loaded
+        if (this.items.length > 0) {
+          return
+        }
+
+        // Items have already been requested
+        if (this.isLoading) {
+          return
+        }
+      }
+
+      this.isLoading = true
+      this.loadingVal = val.toLowerCase()
+
+      // Lazily load input items
+      try {
+        this.entries = await this.$axios.$get(`/api/v1/search?q=${val}`)
+      }
+      catch (err) {
+        console.error(err)
+      }
+      this.isLoading = false
+    },
+    clear () {
+      this.$router.push('/')
     }
   },
   validate ({ params }) {
