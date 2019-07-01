a8b0002dd781345dfd06f0cc5a74bcee53f6b888
Victor Felder <victor@draft.li>
2019-07-01 15:55:49 +0200

    fix: re-enable links to vocabularies

--- components/Term.vue
+++ components/Term.vue
@@ -13,7 +13,12 @@
         </a>
       </div>
       <router-link
-        v-else-if="term.objectIRI && !value.endsWith(':')"
+        v-else-if="term.objectIRI && value.endsWith(':')"
+        :to="{ path: `/prefix/${value}` }">
+        {{ value }}
+      </router-link>
+      <router-link
+        v-else-if="term.objectIRI"
         :to="{ path: `/${value}` }">
         {{ value }}
       </router-link>
