aaa55fd248debb8e95e97dfd8eb1124a727d2d37
Victor Felder <victor@draft.li>
2019-07-09 14:46:36 +0200

    refactor(api): better async init

--- api/index.js
+++ api/index.js
@@ -1,95 +1,109 @@
 const express = require('express')
+require('express-async-errors')
+
 const { cachedShrink, cachedExpand, prepareData } = require('./utils')
 
 const app = express()
 const router = express.Router()
+let initDone = false
+let fuse, searchArrayByPrefix, prefixEndpointData, summary
 
 module.exports = { path: '/api/v1', handler: app }
 
-;(async () => {
-  const { fuse, searchArrayByPrefix, prefixEndpointData, summary } = await prepareData()
+async function init () {
+  if (initDone) {
+    return
+  }
+  ({ fuse, searchArrayByPrefix, prefixEndpointData, summary } = await prepareData())
+  initDone = true
+}
 
-  router.get('/search', (req, res) => {
-    const query = (req.query.q || '').replace(/---hash---/g, '#').trim()
+router.get('/search', async (req, res) => {
+  await init()
+  const query = (req.query.q || '').replace(/---hash---/g, '#').trim()
 
-    if (!query) {
-      res.json([])
+  if (!query) {
+    res.json([])
+    return
+  }
+
+  // detect queries like this: `skos:`, `skos:foo`
+  // do not detect queries containing a URL or spaces
+  if (query.split(' ').length <= 1 && query.split('.').length <= 3 && !query.includes('://')) {
+    const prefix = query.split(':')[0]
+    // scope the search to only this prefix
+    if (searchArrayByPrefix[prefix]) {
+      res.json(searchArrayByPrefix[prefix].search(query).slice(0, 10))
       return
     }
+  }
 
-    // detect queries like this: `skos:`, `skos:foo`
-    // do not detect queries containing a URL or spaces
-    if (query.split(' ').length <= 1 && query.split('.').length <= 3 && !query.includes('://')) {
-      const prefix = query.split(':')[0]
-      // scope the search to only this prefix
-      if (searchArrayByPrefix[prefix]) {
-        res.json(searchArrayByPrefix[prefix].search(query).slice(0, 10))
-        return
-      }
+  res.json(fuse.search(query).slice(0, 10))
+})
+
+router.get('/prefix', async (req, res) => {
+  await init()
+  const query = (req.query.q || '').trim()
+  const prefix = query.split(':')[0]
+
+  if (!prefix || !prefixEndpointData[prefix]) {
+    res.status(404).json([])
+  }
+  res.json(prefixEndpointData[prefix])
+})
+
+router.get('/summary', async (req, res) => {
+  await init()
+  res.json(summary)
+})
+
+router.get('/shrink', async (req, res) => {
+  await init()
+  let iri = req.query.q
+
+  if (iri) {
+    // detect URI encoded `://`
+    if (iri.includes('%3A%2F%2F')) {
+      iri = decodeURIComponent(iri)
     }
-
-    res.json(fuse.search(query).slice(0, 10))
-  })
-
-  router.get('/prefix', (req, res) => {
-    const query = (req.query.q || '').trim()
-    const prefix = query.split(':')[0]
-
-    if (!prefix || !prefixEndpointData[prefix]) {
-      res.status(404).json([])
-    }
-    res.json(prefixEndpointData[prefix])
-  })
-
-  router.get('/summary', (req, res) => {
-    res.json(summary)
-  })
-
-  router.get('/shrink', (req, res) => {
-    let iri = req.query.q
-
-    if (iri) {
-      // detect URI encoded `://`
-      if (iri.includes('%3A%2F%2F')) {
-        iri = decodeURIComponent(iri)
-      }
-      const attempt = cachedShrink({ value: iri })
-      if (attempt !== iri) {
-        return res.json({
-          success: true,
-          value: attempt
-        })
-      }
-      return res.status(404).json({
-        success: false
+    const attempt = cachedShrink({ value: iri })
+    if (attempt !== iri) {
+      return res.json({
+        success: true,
+        value: attempt
       })
     }
+    return res.status(404).json({
+      success: false
+    })
+  }
 
-    res.status(400).json({ help: '/api/v1/shrink?q=…' })
-  })
+  res.status(400).json({ help: '/api/v1/shrink?q=…' })
+})
 
-  router.get('/expand', (req, res) => {
-    const prefixed = req.query.q
+router.get('/expand', async (req, res) => {
+  await init()
+  const prefixed = req.query.q
 
-    if (prefixed) {
-      const attempt = cachedExpand({ value: prefixed })
-      if (attempt !== prefixed) {
-        return res.json({
-          success: true,
-          value: attempt
-        })
-      }
-      return res.status(404).json({
-        success: false
+  if (prefixed) {
+    const attempt = cachedExpand({ value: prefixed })
+    if (attempt !== prefixed) {
+      return res.json({
+        success: true,
+        value: attempt
       })
     }
+    return res.status(404).json({
+      success: false
+    })
+  }
 
-    res.status(400).json({ help: '/api/v1/expand?q=…' })
-  })
+  res.status(400).json({ help: '/api/v1/expand?q=…' })
+})
 
-  router.get('/health', (req, res) => {
-    res.json('ok')
-  })
+router.get('/health', async (req, res) => {
+  await init()
+  res.json('ok')
+})
 
-  app.use(router)
-})()
+app.use(router)
